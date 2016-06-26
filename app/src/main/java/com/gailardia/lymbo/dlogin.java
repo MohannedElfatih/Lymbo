6fb78f9dbb4c3d3210e13bc94603dac1dd1a2280
TerminatorXxX <hmadakiller95@gmail.com>
2016-06-26 03:48:01 +0300

    signin

--- app/src/main/java/com/gailardia/lymbo/dlogin.java
+++ app/src/main/java/com/gailardia/lymbo/dlogin.java
@@ -4,23 +4,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class dlogin extends AppCompatActivity {
-//sssss
+import com.kosalgeek.asynctask.AsyncResponse;
+import com.kosalgeek.asynctask.PostResponseAsyncTask;
+
+import java.util.HashMap;
+
+public class dlogin extends AppCompatActivity implements AsyncResponse {
+    //sssss
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dlogin);
     }
-    public void backToChoices(View view){
-        Intent intent=new Intent(this,choices.class);
+
+    public void backToChoices(View view) {
+        Intent intent = new Intent(this, choices.class);
         startActivity(intent);
     }
-    public void Dsignup(View view){
-        Intent intent=new Intent(this,dsignup.class);
+
+    public void Dsignup(View view) {
+        Intent intent = new Intent(this, dsignup.class);
         startActivity(intent);
     }
-    public void  finishlogin(){
+
+    public void signin(View view) {
+        HashMap map=new HashMap();
+        EditText user=(EditText)findViewById(R.id.loginUser);
+        EditText pass=(EditText)findViewById(R.id.loginPass);
+        String U=user.getText().toString();
+        String P=pass.getText().toString();
+        map.put("Dname",U);
+        map.put("Dpassword",P);
+
+        PostResponseAsyncTask task=new PostResponseAsyncTask(this,map);
+        task.execute("http://lymbo.esy.es/signin.php");
+
+    }
+    public void finishlogin(){
         finish();
     }
+
+    @Override
+    public void processFinish(String s) {
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
 }
+
